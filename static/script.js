404f1ac3550b0ad7b6989324128f0cabfb698ead
Vinicius Koji Enari <viniciusenari@gmail.com>
2022-01-26 01:18:11 -0300

    Added animations

--- static/script.js
+++ static/script.js
@@ -22,3 +22,23 @@ navItem.forEach((item) => {
     }
   });
 });
+
+/* Animate items that have the attribute data-anime */
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+  const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+
+  item.forEach((element) => {
+    if (windowTop > element.offsetTop) {
+      element.classList.add("animate");
+    } else {
+      element.classList.remove("animate");
+    }
+  });
+};
+
+window.addEventListener("scroll", () => {
+  animeScroll();
+});
